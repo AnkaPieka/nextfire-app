722db8041249d6b85c120f099e3e028053d8e144
Anna Lefour <anna.lefour@laposte.net>
2021-07-02 10:50:10 +0200

    Auth hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
 
 // import { GetServerSideProps } from "next";
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider value={{ user: {}, username: "anna" }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
